2d6442f6da5c8c05121749bcd362b4b102ea5a4a
Adam <AdamBCo@me.com>
2019-08-29 09:01:58 -0700

    Completed project

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+
+  app.get( "/filteredimage", async ( req, res ) => {
+    // destruct our path params
+    let { image_url } = req.query;
+
+    if (!image_url) {
+      return res.status(400).send(`image_url is required`);
+    }
+
+    filterImageFromURL(image_url)
+        .then(image => {
+          res.status(200).sendFile(image)
+          res.on('finish', () => deleteLocalFiles([image]));
+        })
+        .catch(error => res.status(error.status).send(error.message));
+  });
+
+  //! END @TODO1
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  } );
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
